1c59e0bb847b9d000b40758c829ae18406b9a734
Ysold <73821223+Ysold@users.noreply.github.com>
2023-11-29 15:46:53 +0100

    première version

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+class NotificationService {
+
+  static void initialize() {
+    FirebaseMessaging.instance.requestPermission();
+
+    FirebaseMessaging.onMessage.listen((event) {
+      print('A new onMessage event was published!');
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+    });
+  }
+
+
+  static Future<String?> getToken() async {
+    return FirebaseMessaging.instance.getToken(vapidKey: "BFegerezgi_Uv2hezgtkqEDTzzertez-Xbt8OK8mJ4eteztVsdWDJQLkMg");
+  }
+
+}
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+
+importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAMYeqrgrqgwKu4kwwIp6BdtE",
+    authDomain: "flutterqgqgr95.firebaseapp.com",
+    projectId: "flutterfireqgfqdfg95",
+    storageBucket: "flutterfireqgdqfgqdfg95.appspot.com",
+    messagingSenderId: "39qrgqdgqerg0",
+    appId: "1:39qgdsgf5230:web:gqdrgf476605ef"
+});
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((m) => {
+  console.log("onBackgroundMessage", m);
+});
